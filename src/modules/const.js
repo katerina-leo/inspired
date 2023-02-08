53edcc2f8c30a8e5feb209fd191d219cb93a6e9c
katerina-leo <81039066+katerina-leo@users.noreply.github.com>
2023-02-08 15:21:49 +0300

    change api url

--- src/modules/const.js
+++ src/modules/const.js
@@ -1,13 +1,13 @@
 export const COUNT_PAGINATION = 3;
 export const DATA = {};
-export const API_URL = 'http://localhost:8024';
+export const API_URL = "https://unmarred-wooded-cafe.glitch.me";
 export const TITLE = {
   women: {
-    title: 'Новая коллекция Бюстгальтер-балконет',
+    title: "Новая коллекция Бюстгальтер-балконет",
     id: "4491988883",
   },
   men: {
     title: "Боксеры из новой коллекции",
-    id: "3215522146"
-  }
-}
+    id: "3215522146",
+  },
+};
